e3a82fb01dcbc5d0ccbed03991c21b8d88e6448f
ardhanubmp <ardhanu.bmp@gmail.com>
2016-09-16 20:39:33 +0700

    first

---
+++ assets/js/custom.js
@@ -0,0 +1,40 @@
+$('#myCarouselSatu').carousel({
+  interval: 4000
+});
+$('#myCarouselDua').carousel({
+  interval: 4000
+});
+
+$('.carousel.satu .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<3;i++) {
+    next=next.next();
+    if (!next.length) {
+    	next = $(this).siblings(':first');
+  	}
+
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
+
+$('.carousel.dua .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<3;i++) {
+    next=next.next();
+    if (!next.length) {
+    	next = $(this).siblings(':first');
+  	}
+
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
